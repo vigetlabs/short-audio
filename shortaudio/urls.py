950dc787519f44701d20c41085543f2461b3bfd3
Paul McKissock <paulsmckissock@gmail.com>
2024-07-23 17:51:14 -0400

    [#2] Removed unnecessary import.

--- shortaudio/urls.py
+++ shortaudio/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from audioapp.views import profile_view
 from django.conf import settings
 from django.conf.urls.static import static
 
