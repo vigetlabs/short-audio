2e18bb2a8f2e516e55323962f9b8fc6b948cedcb
Paul McKissock <paulsmckissock@gmail.com>
2024-07-23 16:55:21 -0400

    [#1] Fixed spacing issue.

--- audioapp/views.py
+++ audioapp/views.py
@@ -6,8 +6,8 @@ import allauth
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+    
 @login_required
-
 def profile_view(request):
     return render(request, 'profile.html', {'username': request.user.username})
 
