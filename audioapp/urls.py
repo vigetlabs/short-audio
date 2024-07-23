083e5e4937d9cdc9226625bf3f24a525496a7452
Paul McKissock <paulsmckissock@gmail.com>
2024-07-23 17:34:17 -0400

    [#2] Fixed some merge issues.

--- audioapp/urls.py
+++ audioapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from . import views
 from .views import upload_audio, audio_detail, user_detail
 
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("accounts/", include("allauth.urls")),
@@ -9,5 +10,4 @@ urlpatterns = [
     path('upload/', upload_audio, name='upload_audio'),
     path('audio/<int:pk>/', audio_detail, name='audio_detail'),
     path('user/<str:username>/', user_detail, name='user_detail'),
-    
 ]
