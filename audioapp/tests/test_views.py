eb5d1287ffd50205f8426a24a34bae05c3048012
Paul McKissock <paulsmckissock@gmail.com>
2024-07-25 12:23:11 -0400

    [#8] Setup previous button.

--- audioapp/tests/test_views.py
+++ audioapp/tests/test_views.py
@@ -10,6 +10,7 @@ class AudioFileViewsTests(TestCase):
         self.client = Client()
         self.user = UserFactory(username="testuser")
         self.audio_file = AudioFileFactory(user=self.user)
+        self.audio_files = [AudioFileFactory(user=self.user) for _ in range(5)]
         self.client.login(username="testuser", password="password")
 
     def test_index_view(self):
@@ -88,3 +89,47 @@ class AudioFileViewsTests(TestCase):
         comment = CommentFactory(user=self.user, audio_file=self.audio_file)
         self.assertIn(comment, self.audio_file.comments.all())
         self.assertIn(comment, Comment.objects.filter(user=self.user))
+
+    #test for you page
+    def test_for_you_initial(self):
+        response = self.client.get(reverse('for_you'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "for_you.html")
+        self.assertIn('audio_file', response.context)
+        self.assertIn('fyp_index', response.context)
+        self.assertIn('reached_end', response.context)
+        self.assertEqual(response.context['fyp_index'], 0)
+        self.assertFalse(response.context['reached_end'])
+
+    def test_for_you_next(self):
+        self.client.get(reverse('for_you'))
+        response = self.client.get(reverse('for_you'), {'action': 'next'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "for_you.html")
+        self.assertIn('audio_file', response.context)
+        self.assertIn('fyp_index', response.context)
+        self.assertIn('reached_end', response.context)
+        self.assertEqual(response.context['fyp_index'], 1)
+
+    def test_for_you_previous(self):
+        self.client.get(reverse('for_you')) 
+        self.client.get(reverse('for_you'), {'action': 'next'})
+        response = self.client.get(reverse('for_you'), {'action': 'previous'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "for_you.html")
+        self.assertIn('audio_file', response.context)
+        self.assertIn('fyp_index', response.context)
+        self.assertIn('reached_end', response.context)
+        self.assertEqual(response.context['fyp_index'], 0)
+
+    def test_for_you_reached_end(self):
+        self.client.get(reverse('for_you'))
+        for _ in range(len(self.audio_files) - 1):
+            self.client.get(reverse('for_you'), {'action': 'next'})
+        response = self.client.get(reverse('for_you'), {'action': 'next'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "for_you.html")
+        self.assertIn('audio_file', response.context)
+        self.assertIn('fyp_index', response.context)
+        self.assertIn('reached_end', response.context)
+        self.assertTrue(response.context['reached_end'])
