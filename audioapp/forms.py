f9a74b31f950d22b86d61ba7efe96beadaf2deaa
Paul McKissock <paulsmckissock@gmail.com>
2024-07-29 13:44:39 -0400

    [#N/A] Styled For You Page. Added login and signup templates. Added has_liked variable to for you view

--- audioapp/forms.py
+++ audioapp/forms.py
@@ -12,3 +12,11 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["text"]
+        widgets = {
+            'text': forms.Textarea(attrs={
+                'rows': 5,  # Number of rows
+                'cols': 30,  # Number of columns
+                'class': 'w-full p-2 border border-gray-300 rounded',  # Tailwind CSS classes
+                'placeholder': 'Write your comment here...',
+            }),
+        }
